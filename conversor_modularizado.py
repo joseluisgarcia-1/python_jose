460d420b0a5ca5413c0b5bea185bef3e26ffb7ff
joseluisgarcia-1 <jlgarcia002@misena.edu.co>
2021-11-12 09:29:19 -0500

    COMMIT IMPORTANTE explicación de funcioes

--- conversor_modularizado.py
+++ conversor_modularizado.py
@@ -1,5 +1,5 @@
-def conversor(numero_pesos, valor_dolar):
-    pesos = float(input("¿Cuántos pesos "+ numero_pesos +" tienes?: "))
+def conversor(tipo_pesos, valor_dolar):
+    pesos = float(input("¿Cuántos pesos "+ tipo_pesos +" tienes?: "))
     dolares = pesos/valor_dolar
     dolares_round = round(dolares, 2)
     dolares_caracteres = str(dolares_round)
@@ -11,6 +11,7 @@ su correspondiente número
 3- Pesos argentinos
 4- Pesos bolivianos
  """
+
 print(menu)
 opcion = int(input("Ingresa tu opción: "))
 print("Escogiste la opcion", opcion)
@@ -25,3 +26,8 @@ elif opcion == 4:
     conversor("bolivianos", 7)
 else:
     print("Elige una opción válida")
+
+""""lo que entendí de esto hasta el momento es que, el conversor con los parametros recibe el tipo de pesos o de nacionalidad y el parametro
+valor dolar es el valor del cada dolar en cada nacionalidad o moneda, por ejemplo en la opcion uno tenemos que, en la moneda de los colombianos
+cada dolar cuesta 3879 pesos, por esta razón decimos que ingrese cuantos pesos tiene para dividir esos pesos de la nacionalidad, entre el valor
+del dolar de cada nacionalidad correspondiente"""
