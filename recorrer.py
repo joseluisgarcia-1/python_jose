1620e855246973bac08ec3f93d8e1e5a54710e03
joseluisgarcia-1 <jlgarcia002@misena.edu.co>
2021-11-12 20:25:07 -0500

    ciclos

---
+++ recorrer.py
@@ -0,0 +1,12 @@
+def run():
+    """nombre = str(input("Ingresa un nombre: "))
+    for i in nombre:
+        print(i)"""
+    
+    frase = str(input("Escribe una cadena de caracteres: "))
+    for i in frase:
+        print(i.lower())
+    
+
+if __name__ == '__main__':
+    run()
